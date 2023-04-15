5e68bf464f69090184429b5c879712cc3fdded9e
Cassio <superkoalabr@gmail.com>
2023-04-15 15:13:15 -0300

    80%

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,14 @@
+import re
+from tech_news.database import db
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    noticias = db.news.find({"title": {"$regex": title, "$options": "i"}})
+    noticias_lista = [
+        (noticia["title"], noticia["url"]) for noticia in noticias
+    ]
+    return noticias_lista
 
 
 # Requisito 8
@@ -10,4 +18,7 @@ def search_by_date(date):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+
+    regex = re.escape(category)
+    news = db.news.find({"category": {"$regex": regex, "$options": "i"}})
+    return [(noticia["title"], noticia["url"]) for noticia in news]
